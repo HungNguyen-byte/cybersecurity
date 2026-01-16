e50e28eb6068e544ff2777b75e1fd0c48bb40953
Nguyen Tuan Hung <hungquang246810@gmail.com>
2026-01-16 07:41:44 +0700

    hybrid cryptography

--- utils.h
+++ utils.h
@@ -11,13 +11,13 @@ using bytes = std::vector<unsigned char>;
 
 inline bytes read_file(const std::string& path) {
     std::ifstream f(path, std::ios::binary);
-    if (!f) throw std::runtime_error("Cannot open file: " + path);
+    if (!f) throw std::runtime_error("Cannot open file / folder: " + path);
     return bytes((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
 }
 
 inline void write_file(const std::string& path, const bytes& data) {
     std::ofstream f(path, std::ios::binary);
-    if (!f) throw std::runtime_error("Cannot write file: " + path);
+    if (!f) throw std::runtime_error("Cannot write file / folder: " + path);
     f.write(reinterpret_cast<const char*>(data.data()), data.size());
 }
 
